8031bc8cb13bd570ec277bbb06c399aad0679499
Tom Gregor <tgregor@ivsoftware.com>
2025-01-21 09:16:38 -0700

    Add project files.

---
+++ dgv-sorting-and-async/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace dgv_sorting_and_async
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            buttonUpdate = new Button();
+            dataGridView = new DataGridView();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(dataGridView, 0, 0);
+            tableLayoutPanel1.Controls.Add(buttonUpdate, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(10, 10);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Size = new Size(558, 524);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.BackColor = Color.CornflowerBlue;
+            buttonUpdate.Dock = DockStyle.Right;
+            buttonUpdate.ForeColor = Color.White;
+            buttonUpdate.Location = new Point(358, 477);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new Size(197, 44);
+            buttonUpdate.TabIndex = 1;
+            buttonUpdate.Text = "UPDATE";
+            buttonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AllowUserToResizeColumns = false;
+            dataGridView.AllowUserToResizeRows = false;
+            dataGridView.BackgroundColor = Color.Azure;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(3, 3);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.RowHeadersWidth = 62;
+            dataGridView.Size = new Size(552, 468);
+            dataGridView.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 544);
+            Controls.Add(tableLayoutPanel1);
+            Name = "MainForm";
+            Padding = new Padding(10);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Form";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridView;
+        private Button buttonUpdate;
+    }
+}
---
+++ dgv-sorting-and-async/MainForm.cs
@@ -0,0 +1,141 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace dgv_sorting_and_async
+{
+    public partial class MainForm : Form
+    {
+        private readonly ClientMachine _clientMachine = new ClientMachine();
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonUpdate.Click += async(sender, e) =>
+            {
+                var syslogTask = _clientMachine.QuerySYSLOG();
+                var yumStatusTask = _clientMachine.QueryYumStatus();
+                await Task.WhenAll(syslogTask, yumStatusTask);
+                var syslogResponse = await syslogTask;
+                var yumResponse = await yumStatusTask;
+                if (syslogResponse != null && yumResponse != null)
+                {
+                    var syslogRecords = await ParseResponseAsync(syslogResponse);
+                    var yumRecords = await ParseResponseAsync(yumResponse);
+                    // Initial sort
+                    Records.Clear();
+                    foreach(var record in 
+                        syslogRecords.Concat(yumRecords)
+                       .OrderBy(record => record.Timestamp))
+                    {
+                        Records.Add(record);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("One or both queries failed.", "Update Failed");
+                }
+            };
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            dataGridView.DataSource = Records;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView.Columns[nameof(LogRecord.Description)].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+        IList Records { get; } = new SortableBindingList<LogRecord>();
+        private async Task<List<LogRecord>> ParseResponseAsync(HttpResponseMessage response)
+        {
+            if( response.IsSuccessStatusCode && 
+                JsonConvert
+                .DeserializeObject<List<LogRecord>>(await response.Content.ReadAsStringAsync()) is { } records)
+            {
+                return records;
+            }
+            else return  new List<LogRecord>();
+        }
+    }
+    class ClientMachine
+    {
+        Random _rando = new Random(1);
+        public async Task<HttpResponseMessage?> QuerySYSLOG() 
+        {
+            await Task.Delay(TimeSpan.FromSeconds(_rando.NextDouble()));    // Simulated query time
+            var records = new[]
+            { "Server started", "Connection lost", "Recovered", "Warning issued", "All systems operational" }
+            .Select(_ => new LogRecord
+            {
+                Type = LogType.SYSLOG,
+                Description = _,
+                Timestamp = DateTime.UtcNow.AddMinutes(-_rando.Next(5, 60)),
+            });
+
+            var jsonContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(records), System.Text.Encoding.UTF8, "application/json");
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = jsonContent };
+
+        }
+        public async Task<HttpResponseMessage?> QueryYumStatus()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(_rando.NextDouble()));
+            var records = new[]
+            { "Version check passed", "Update required", "Critical update available", "Version up-to-date", "Unknown version error" }
+            .Select(_ => new LogRecord
+            {
+                Type = LogType.VERSION_CHECK,
+                Description = _,
+                Timestamp = DateTime.UtcNow.AddMinutes(-_rando.Next(5, 60)),
+            });
+
+            var jsonContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(records), System.Text.Encoding.UTF8, "application/json");
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = jsonContent };
+
+        }
+    }
+    public enum LogType{ SYSLOG, VERSION_CHECK }
+    public class LogRecord
+    {
+        public DateTime Timestamp { get; set; }
+        public LogType Type { get; set; }
+        public string? Description { get; set; }
+    }
+    public class SortableBindingList<T> : BindingList<T>
+    {
+        protected override void ApplySortCore(PropertyDescriptor prop, ListSortDirection direction)
+        {
+            if (prop != null)
+            {
+                var items = this.Items as List<T>;
+                if (items != null)
+                {
+                    var propValue = prop.GetValue;
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        items.Sort((x, y) => Comparer<object>.Default.Compare(propValue(x), propValue(y)));
+                    }
+                    else
+                    {
+                        items.Sort((x, y) => Comparer<object>.Default.Compare(propValue(y), propValue(x)));
+                    }
+                    _sortProperty = prop;
+                    _sortDirection = direction;
+                    _isSorted = true;
+                }
+            }
+            else
+            {
+                _isSorted = false;
+            }
+            ResetBindings();
+        }
+        private bool _isSorted;
+        private PropertyDescriptor? _sortProperty;
+        private ListSortDirection _sortDirection;
+        protected override bool SupportsSortingCore => true;
+        protected override bool IsSortedCore => _isSorted;
+        protected override ListSortDirection SortDirectionCore => _sortDirection;
+        protected override PropertyDescriptor? SortPropertyCore => _sortProperty;
+        protected override void RemoveSortCore() => _isSorted = false;
+    }
+}
